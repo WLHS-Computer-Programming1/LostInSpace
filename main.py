1c6fcc97ca4f4c69aea1258aa7c3a79e1ea7aa51
smithbwlhs <smithb@wlwv.k12.or.us>
2025-01-08 10:00:02 -0800

    updated

--- main.py
+++ main.py
@@ -4,47 +4,94 @@ Date: 10/3/24
 Description: This is a text adventure game called Lost in Space about someone who crash lands on a planet
 and tries to find their way around the planet.
 '''
+import random
 import time # for slow scrolling
 
-print("Possible Themes".center(40,'-'))
-print("Castle")
-print("Swamp")
-print("Space crash")
+def print_intro_text():
+    # raw text needed for ASCII art, use r in front of '''
+    intro_art = r'''
+    
+                         `. ___
+                        __,' __`.                _..----....____
+            __...--.'``;.   ,.   ;``--..__     .'    ,-._    _.-'
+      _..-''-------'   `'   `'   `'     O ``-''._   (,;') _,'
+    ,'________________                          \`-._`-','
+     `._              ```````````------...___   '-.._'-:
+        ```--.._      ,.                     ````--...__\-.
+                `.--. `-`                       ____    |  |`
+                  `. `.                       ,'`````.  ;  ;`
+                    `._`.        __________   `.      \'__/`
+                       `-:._____/______/___/____`.     \  `
+                                   |       `._    `.    \
+                                   `._________`-.   `.   `.___
+                                                 SSt  `------'`
+    '''
+    
+    print("Welcome to Lost in Space.....")
+    # could I slowly scroll an empty screen until the ship appears?
+    for i in range(10):
+        print(".")
+        time.sleep(0.5)
+    print(intro_art)
 
-# raw text needed for ASCII art, use r in front of '''
-intro_art = r'''
+def crash_landing_scene():
+    initial_health = random.randint(50,90)
+    player_info = {
+        'name':'hal',
+        'health': initial_health
+    }
+    print("You feel foggy as you slowly wake up to the sound of alarms and the smell of smoke.")
+    print("You look down at your suit and see that your name is ")
+    print("(Enter your name)")
+    player_name = input("> ")
+    player_info['name'] = player_name
+    print(f"Checking your vitals, you see that your overall health is at {player_info['health']}%")
+    print(f"You look around and realize you have crashed on an unknown planet. Stepping outside, you "
+          f"see a damaged escape pod, what looks like a signal tower in distance, and a forest.")
+    print(f"Do you\n1) Explore your damaged ship more"
+          f"\n2)Investigate the signal tower"
+          f"\n3)Examine the damaged pod"
+          f"\n4)Enter the forest")
+    while True:
+        try:
+            player_choice = int(input("> "))
+        except ValueError:
+            print("That is not a valid option")
+            continue
+        if player_choice == 1:
+            explore_ship(player_info)
+        elif player_choice == 2:
+            signal_tower_encounter(player_info)
+        elif player_choice == 3:
+            damaged_pod_encounter(player_info)
+        elif player_choice == 4:
+            forest_encounter(player_info)
+        else:
+            print("Invalid choice")
+            continue
 
-                     `. ___
-                    __,' __`.                _..----....____
-        __...--.'``;.   ,.   ;``--..__     .'    ,-._    _.-'
-  _..-''-------'   `'   `'   `'     O ``-''._   (,;') _,'
-,'________________                          \`-._`-','
- `._              ```````````------...___   '-.._'-:
-    ```--.._      ,.                     ````--...__\-.
-            `.--. `-`                       ____    |  |`
-              `. `.                       ,'`````.  ;  ;`
-                `._`.        __________   `.      \'__/`
-                   `-:._____/______/___/____`.     \  `
-                               |       `._    `.    \
-                               `._________`-.   `.   `.___
-                                             SSt  `------'`
-'''
 
-print("Welcome to Lost in Space.....")
-# could I slowly scroll an empty screen until the ship appears?
-for i in range(10):
-    print(".")
-    time.sleep(0.5)
-print(intro_art)
-print("You feel foggy as you slowly wake up to the sound of alarms and the smell of smoke.")
-print(f"1. Take off your helmet\n2. Check your vitals\n")
-choice_one = int(input("> "))
-if choice_one == 1:
-    print("You immediately regret this decision as the acrid smoke causes you to choke")
-elif choice_one == 2:
-    print("Your vitals are stable for now, but some are concerning.")
-else:
-    print("You made an unwise choice and everything fades to black.")
+def explore_ship(player_info):
+    pass
+
+def signal_tower_encounter(player_info):
+    pass
+
+def damaged_pod_encounter(player_info):
+    pass
+
+def forest_encounter(player_info):
+    pass
+
+
+
+
+def main():
+    print_intro_text()
+    crash_landing_scene()
+
+if __name__ == '__main__':
+    main()
 
 
 
--- useful_tools.py
+++ useful_tools.py
@@ -12,6 +12,22 @@ full explore in class or be tested on
 # Location, called index, starts at 0
 # To get first item, my_list[0]
 
+character_info = ["Norman",7,"Cat","Black","Yellow",4,"Quirky"]
+for item in character_info:
+    print(item)
+
+name = character_info[0]
+print(f"Name: {name}")
+num_legs = character_info[5]
+print(f"Number of legs: {num_legs}")
+
+def leg_change(info,leg_number):
+    info[5] = leg_number
+    return info
+
+character_info = leg_change(character_info,3)
+num_legs = character_info[5]
+print(f"Number of legs: {num_legs}")
 
 
 # Dictionary
@@ -19,6 +35,19 @@ full explore in class or be tested on
 # be a word, not a number
 # Format: my_dictionary = {key1:value1,key2:value2,key3:value3} etc.
 # To get an item, my_dictionary[key]
+
+character_dictionary = {
+    "Name":"Norman",
+    "Age":7,
+    "Species":"Cat",
+    "Color":"Black",
+    "Eye Color":"Yellow",
+    "num_legs":4,
+    "adj_1":"Quirky",
+    "inventory":["key","meds","flashlight"]
+}
+print(character_dictionary["Species"])
+
 
 # Sleep
 # import sleep is needed at top; delays next line of code for specified time
